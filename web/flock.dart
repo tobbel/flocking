658430b123f7ad1576171878b9bdd7ab69a40d56
Tobias Lilja <tobias.lilja@gmail.com>
2014-06-16 22:29:47 +0200

    Started rendering boids as triangles
    This code is so good looking, it -has- to work!

--- web/flock.dart
+++ web/flock.dart
@@ -2,7 +2,7 @@ part of flocking;
 
 class Flock {
   List<Boid> boids;
-  static const int NUM_BOIDS = 100;
+  static const int NUM_BOIDS = 10;
   final Vector2 worldSize;
   
   Flock(this.worldSize) {
--- web/renderer_2d.dart
+++ web/renderer_2d.dart
@@ -10,13 +10,43 @@ class Renderer2D {
     // Temp render boids
     flock.boids.forEach((b) {
       // TODO: Move canvas to center, rotate, draw triangle (see fractals)
-      context.fillRect(b.position.x, b.position.y, 2, 2);
-      
-      // Temp draw neighbor distance (made for a cool pattern!)
       context.beginPath();
-      context.arc(b.position.x, b.position.y, Boid.NEIGHBORHOOD_DISTANCE,     0, Math.PI*2, false);
-      context.arc(b.position.x, b.position.y, Boid.NEIGHBORHOOD_DISTANCE - 1, 0, Math.PI*2, true);
-      context.fill();
+      Vector2 direction = b.velocity.normalized();
+      Vector2 perpDir = new Vector2(-direction.y, direction.x);
+      perpDir *= 15.0; // base is 10 long
+      double x = b.position.x + perpDir.x;
+      double y = b.position.y + perpDir.y;
+      context.moveTo(x, y);
+      x -= perpDir.x * 2.0;
+      y -= perpDir.y * 2.0;
+      context.lineTo(x, y);
+      context.stroke();
+      
+      // Current angle
+      double angle = Math.atan2(y, x);
+      // Add 250 deg
+      angle += 250 * Math.PI / 180.0;
+      Vector2 oldPos = new Vector2(x, y);
+      // Back to vector again
+      double newx = Math.cos(angle);
+      double newy = Math.sin(angle);
+      Vector2 dirVector = new Vector2(newx, newy).normalized();
+      dirVector *= 30.0;
+      Vector2 newPos = oldPos + dirVector; 
+      context.lineTo(newPos.x, newPos.y);
+      context.stroke();
+      
+      angle = Math.atan2(y, x);
+      angle += 40 * Math.PI / 180.0;
+      newx = Math.cos(angle);
+      newy = Math.sin(angle);
+      dirVector = new Vector2(newx, newy).normalized();
+      dirVector *= 30.0;
+      newPos += dirVector;
+      context.lineTo(newPos.x, newPos.y);
+      context.stroke();
+      
+      //context.fillRect(b.position.x, b.position.y, 2, 2);
     });
   }
 }
